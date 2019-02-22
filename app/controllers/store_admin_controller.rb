7a83a6a098a0980039a222c224923ce27bd4c93f
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-22 23:57:29 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,15 +1,15 @@
 class StoreAdminController < ApplicationController
 
   def list
-    render :layout => "admin/home"
+    render :layout => "admin"
   end
 
   def order
-    render :layout => :order_administration => "orders"
+    render :layout => "order_administration"
+    # render :order_administration => "orders"
   end
 
   def invoice
     render :layout => false
-    # render :store_admin => "invoice"
   end
 end
